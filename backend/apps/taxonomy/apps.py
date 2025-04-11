19c6c04372a6100ec8f41833e02d368f45d07668
dr-rompecabezas <felavid@gmail.com>
2025-04-11 00:00:23 -0400

    restructure apps and models for biodiversity, taxonomy, and reports
    - Renamed `apps/trees` to `apps/biodiversity` and updated models to
      reflect biodiversity-specific data.
    - Renamed  `apps/observations` to `apps/reports` and updated models
      for measurements and observations.
    - Added a new `apps/taxonomy` app to manage taxonomic data,
      including families, genera, and species.
    - Updated migrations to reflect the new structure and relationships.
    - Removed deprecated models and migrations from `apps/observations`
      and `apps/trees`.

--- backend/apps/taxonomy/apps.py
+++ backend/apps/taxonomy/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ObservationsConfig(AppConfig):
+class TaxonomyConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.observations"
+    name = "apps.taxonomy"

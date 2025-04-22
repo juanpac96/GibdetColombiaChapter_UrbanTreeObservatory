30a051cf9eed047af2babcbac8694124d381b62e
dr-rompecabezas <felavid@gmail.com>
2025-04-22 11:10:25 -0400

    feat: add URL routing for reports app

--- backend/config/api_urls.py
+++ backend/config/api_urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     path("taxonomy/", include("apps.taxonomy.urls")),
     path("places/", include("apps.places.urls")),
     path("biodiversity/", include("apps.biodiversity.urls")),
-    # path('reports/', include('apps.reports.urls')),
+    path("reports/", include("apps.reports.urls")),
 ]

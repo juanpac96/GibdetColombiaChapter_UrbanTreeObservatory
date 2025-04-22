39646cdbf5b716820885e7da80511f37548949e5
dr-rompecabezas <felavid@gmail.com>
2025-04-22 10:54:36 -0400

    feat: add API schema endpoints and include api_urls for modular routing

---
+++ backend/config/api_urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path("taxonomy/", include("apps.taxonomy.urls")),
+    # path('places/', include('apps.places.urls')),
+    # path('biodiversity/', include('apps.biodiversity.urls')),
+    # path('reports/', include('apps.reports.urls')),
+]

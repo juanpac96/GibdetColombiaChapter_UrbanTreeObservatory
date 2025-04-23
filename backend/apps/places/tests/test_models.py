422527a28a956381039e0eadb71685ccf4e2cb2b
dr-rompecabezas <felavid@gmail.com>
2025-04-22 21:09:12 -0400

    chore(tests): lint and rename test file for places model
    Renamed `test_model.py` to `test_models.py` for consistency.
    Applied linting to improve code readability and maintainability.

--- backend/apps/places/tests/test_models.py
+++ backend/apps/places/tests/test_models.py
@@ -7,22 +7,23 @@ class CountryModelTestCase(TestCase):
 
     def setUp(self):
         """Set up a country instance for testing."""
-        # Note the migrations are also ran when tests are executed, 
+        # Note the migrations are also ran when tests are executed,
         # so there is no need to create the country instance here.
         # self.country = Country.objects.create(name="Colombia")
-        pass 
+        pass
 
     def test_country_str(self):
         """Test the string representation of the country."""
         colombia = Country.objects.get(name="Colombia")
         self.assertEqual(str(colombia), "Colombia")
 
+
 class DepartmentModelTestCase(TestCase):
     """Test case for the Department model."""
 
     def setUp(self):
         """Set up a department instance for testing."""
-        # Note the migrations are also ran when tests are executed, 
+        # Note the migrations are also ran when tests are executed,
         # so there is no need to create the department instance here.
         # colombia = Country.objects.get(name="Colombia")
         # self.department = Department.objects.create(name="Antioquia", country=colombia)
@@ -32,6 +33,7 @@ class DepartmentModelTestCase(TestCase):
         """Test the string representation of the country."""
         department = Department.objects.get(name="Tolima")
         self.assertEqual(str(department), "Tolima, Colombia")
+
 
 class MunicipalityModelTestCase(TestCase):
     """Test case for the Municipality model."""
@@ -48,18 +50,19 @@ class MunicipalityModelTestCase(TestCase):
         municipality = Municipality.objects.get(name="Ibagué")
         self.assertEqual(str(municipality), "Ibagué, Tolima")
 
-class PlaceModelTestCase(TestCase): 
+
+class PlaceModelTestCase(TestCase):
     """Test case for the Place model."""
 
     def setUp(self):
         """Set up a place instance for testing."""
         municipality = Municipality.objects.get(name="Ibagué")
         self.place = Place.objects.create(
-            municipality=municipality, 
-            site="Parque Centenario", 
-            populated_center="Ricaute", 
-            zone=1, 
-            subzone=2
+            municipality=municipality,
+            site="Parque Centenario",
+            populated_center="Ricaute",
+            zone=1,
+            subzone=2,
         )
 
     def test_place_str(self):

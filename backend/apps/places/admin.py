50626d68e94fc90a588de3e9fc424d5325f97644
dr-rompecabezas <felavid@gmail.com>
2025-04-29 08:37:28 -0400

    feat: add Locality and Neighborhood admin models

--- backend/apps/places/admin.py
+++ backend/apps/places/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Country, Department, Municipality, Site
+from .models import Country, Department, Municipality, Locality, Neighborhood, Site
 
 
 @admin.register(Country)
@@ -24,6 +24,32 @@ class MunicipalityAdmin(admin.ModelAdmin):
     list_filter = ("department__country", "department")
     search_fields = ("name", "department__name")
     readonly_fields = ("id",)
+
+
+@admin.register(Locality)
+class LocalityAdmin(admin.ModelAdmin):
+    list_display = ("name", "municipality")
+    list_filter = (
+        "municipality__department__country",
+        "municipality__department",
+        "municipality",
+    )
+    search_fields = ("name", "municipality__name")
+    readonly_fields = ("id", "created_at", "updated_at", "uuid")
+    list_per_page = 25
+
+
+@admin.register(Neighborhood)
+class NeighborhoodAdmin(admin.ModelAdmin):
+    list_display = ("name", "locality")
+    list_filter = (
+        "locality__municipality__department__country",
+        "locality__municipality__department",
+        "locality__municipality",
+    )
+    search_fields = ("name", "locality__name")
+    readonly_fields = ("id", "created_at", "updated_at", "uuid")
+    list_per_page = 25
 
 
 @admin.register(Site)
